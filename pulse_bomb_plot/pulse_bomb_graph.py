ee68951bd79b72fac86019d9dac428c816a974f1
Ryan Davis <ryandavis@ryans-mbp-2.attlocal.net>
2022-01-02 02:24:52 -0500

    more cleanup

--- pulse_bomb_plot/pulse_bomb_graph.py
+++ pulse_bomb_plot/pulse_bomb_graph.py
@@ -20,7 +20,6 @@ tracer = season4[season4['hero_name'] == 'Tracer']
 # Generate a list of player names and the team they play for
 players_and_teams = tracer[['player_name', 'team_name']].groupby('player_name').head(1).reset_index()
 
-
 # Filter for only the stats we care about
 pulse_bomb_stats = ['Ultimates Used', 'Pulse Bomb Kills', 'Pulse Bombs Attached']
 tracer = tracer[tracer['stat_name'].isin(pulse_bomb_stats)]
@@ -46,6 +45,7 @@ def flatten_group(group):
         'Ultimates Used': get_first_or_zero(used)
     })
 
+
 # Sum up each stat for each player, then flatted the stats for each player
 pb_stats = tracer[['player_name', 'stat_name', 'stat_amount']].groupby(
     by=['player_name', 'stat_name']).sum().reset_index().groupby(by=['player_name']).apply(flatten_group).reset_index()
@@ -53,6 +53,8 @@ pb_stats = tracer[['player_name', 'stat_name', 'stat_amount']].groupby(
 # Calculate attach and kill rates
 pb_stats['Attach Rate'] = pb_stats['Pulse Bombs Attached'] / pb_stats['Ultimates Used']
 pb_stats['Kill Rate'] = pb_stats['Pulse Bomb Kills'] / pb_stats['Ultimates Used']
+pb_stats['Kills Per Stick'] = pb_stats['Pulse Bomb Kills'] / pb_stats['Pulse Bombs Attached']
+
 
 # join with list of players and teams to get each player's team for plotting
 pb_stats = pb_stats.merge(players_and_teams, on='player_name')
@@ -60,7 +62,40 @@ pb_stats = pb_stats.merge(players_and_teams, on='player_name')
 pb_stats = pb_stats.sort_values(by='Ultimates Used', ascending=False)
 pb_stats = pb_stats[pb_stats['Ultimates Used'] > min_pulse_bombs_used]
 
-# Plot each player
+# Plot each player for kills per stick
+plt.figure(figsize=(10, 8))
+plt.title('Pulse Bomb Kills Per Attach vs Attach Rate')
+for ind in pb_stats.index:
+    row = pb_stats.loc[ind, :]
+    label = row['player_name']
+    attach_rate = row['Attach Rate']
+    kill_rate = row['Kills Per Stick']
+    used = row['Ultimates Used']
+    team = row['team_name']
+    color = Teams.TeamColors[team]
+    plt.scatter(attach_rate, kill_rate, label=label, s=used * 2, color=color)
+
+# Label each player
+for ind in pb_stats.index:
+    row = pb_stats.loc[ind, :]
+    label = row['player_name']
+    attach_rate = row['Attach Rate']
+    kill_rate = row['Kills Per Stick']
+    team = row['team_name']
+    color = Teams.TeamColors[team]
+    plt.text(attach_rate + 0.005, kill_rate + 0.005, label, color=color, weight='bold')
+
+plt.text(0.53, 0.51, '*Dot size based on \nnumber of pulse bombs used', weight='bold', size='x-small')
+#
+plt.xlim((0.3, 0.6))
+plt.ylim((0.5, 1.5))
+
+plt.xlabel('Pulse Bomb Attach Rate')
+plt.ylabel('Pulse Bomb Kills Per Attach')
+
+plt.savefig('./plots/pulse_bombs_kills_per_stick.png')
+
+# Plot each player Again with kill Rate
 plt.figure(figsize=(10, 8))
 plt.title('Pulse Bomb Kill Rate vs Attach Rate')
 for ind in pb_stats.index:
@@ -71,8 +106,7 @@ for ind in pb_stats.index:
     used = row['Ultimates Used']
     team = row['team_name']
     color = Teams.TeamColors[team]
-    plt.scatter(attach_rate, kill_rate, label=label, s=used*2, color=color)
-
+    plt.scatter(attach_rate, kill_rate, label=label, s=used * 2, color=color)
 
 # Label each player
 for ind in pb_stats.index:
@@ -82,14 +116,14 @@ for ind in pb_stats.index:
     kill_rate = row['Kill Rate']
     team = row['team_name']
     color = Teams.TeamColors[team]
-    plt.text(attach_rate + 0.004, kill_rate + 0.004, label, color=color, weight='bold')
+    plt.text(attach_rate + 0.005, kill_rate + 0.005, label, color=color, weight='bold')
 
-
-x_label = pb_stats['Attach Rate'].max() - 0.05
-y_label = pb_stats['Kill Rate'].min() + 0.005
-plt.text(x_label, y_label, '*Dot size based on \nnumber of pulse bombs used', weight='bold', size='x-small')
-
+plt.text(0.53, 0.31, '*Dot size based on \nnumber of pulse bombs used', weight='bold', size='x-small')
+#
+plt.xlim((0.3, 0.6))
+plt.ylim((0.3, 0.6))
 
 plt.xlabel('Pulse Bomb Attach Rate')
 plt.ylabel('Pulse Bomb Kill Rate')
-plt.show()
+
+plt.savefig('./plots/pulse_bombs_kill_rate.png')
